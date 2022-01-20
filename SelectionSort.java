c7055510240db1f048a33402b0b9c01050b55922
David Chu <92391420+ngkhanggg@users.noreply.github.com>
2022-01-20 17:00:38 +0000

    Added ascending and descending order

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,5 @@
 public class SelectionSort extends Sorter {
-    public int[] sort(int[] unsortedArr) {
+    public int[] ascendingSort(int[] unsortedArr) {
         long start = System.currentTimeMillis(); // starting time
 
 		// count the number of comparisons
@@ -11,6 +11,38 @@ public class SelectionSort extends Sorter {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                count++;
+            }
+        }
+
+        long end = System.currentTimeMillis(); // ending time
+
+		// TEMPPORARY
+		// pass the variables to the parent class
+		// display duration and comparisons
+        super.printExecutionTime(arr.length, end - start);
+        System.out.println();
+        super.printNumOfComparisons(arr.length, count);
+
+        return arr;
+    }
+
+	public int[] descendingSort(int[] unsortedArr) {
+        long start = System.currentTimeMillis(); // starting time
+
+		// count the number of comparisons
+        int count = 0;
+
+		// clone/copy elements from the original array
+        int[] arr = unsortedArr.clone();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
