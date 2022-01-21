ecb8847b655b16291524548a04d7be17b5df4a2f
James Wigg <92392145+S2JNobody@users.noreply.github.com>
2022-01-21 02:55:58 +0000

    Fixed the indentation in main.java

--- Main.java
+++ Main.java
@@ -11,59 +11,60 @@ public class Main {
 	private static SelectionSort selection = new SelectionSort();
 	private static QuickSort quick = new QuickSort();
 
-    public static void main(String[] args) {
-        int[] unsortedArr = generateArr(10); // initialize unsorted array
+  public static void main(String[] args) {
 
-		// display all the numbers in the unsorted array
-        for (int num: unsortedArr) {
-            System.out.print(num + " ");
-        }
-		System.out.println();
+    int[] unsortedArr = generateArr(10); // initialize unsorted array
 
-		// initialize sorted array
-        int[] sortedArr1 = useSort(unsortedArr, "bubble", "ascending");
-		System.out.println();
-        int[] sortedArr2 = useSort(unsortedArr, "selection", "descending");
-
-		// display all the numbers in the sorted array
-		System.out.println();
-        for (int num: sortedArr1) {
-            System.out.print(num + " ");
-        }
-		System.out.println();
-
-		for (int num: sortedArr2) {
-            System.out.print(num + " ");
-        }
+    // display all the numbers in the unsorted array
+    for (int num: unsortedArr) {
+        System.out.print(num + " ");
     }
+    System.out.println();
+
+    // initialize sorted array
+    int[] sortedArr1 = useSort(unsortedArr, "bubble", "ascending");
+    System.out.println();
+    int[] sortedArr2 = useSort(unsortedArr, "selection", "descending");
+
+    // display all the numbers in the sorted array
+    System.out.println();
+    for (int num: sortedArr1) {
+        System.out.print(num + " ");
+    }
+    System.out.println();
+
+    for (int num: sortedArr2) {
+        System.out.print(num + " ");
+    }
+  }
 
 	// generate an array with random numbers
-    public static int[] generateArr(int length) {
-        Random rd = new Random();
+  public static int[] generateArr(int length) {
+      Random rd = new Random();
 
-        int[] arr = new int[length];
+      int[] arr = new int[length];
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = rd.nextInt(99) + 1;
-        }
+      for (int i = 0; i < arr.length; i++) {
+          arr[i] = rd.nextInt(20000) - 10000;
+      }
 
-        return arr;
-    }
+      return arr;
+  }
 
 	// use a particular type of sortings using switch
-    public static int[] useSort(int[] arr, String type, String order) {
-        switch(type) {
-			case "bubble":
-				System.out.println("Bubble Sort");
-				return bubble.sort(arr, order);
-			case "selection":
-				System.out.println("Selection Sort");
-				return selection.sort(arr, order);
-			case "quick":
-				System.out.println("Quick Sort");
-				return quick.sort(arr, order);
-			default:
-				return arr;
-        }
-    }
+  public static int[] useSort(int[] arr, String type, String order) {
+      switch(type) {
+    case "bubble":
+      System.out.println("Bubble Sort");
+      return bubble.sort(arr, order);
+    case "selection":
+      System.out.println("Selection Sort");
+      return selection.sort(arr, order);
+    case "quick":
+      System.out.println("Quick Sort");
+      return quick.sort(arr, order);
+    default:
+      return arr;
+      }
+  }
 }
