5a3d009ed604d4c91865f8b01407518a1c53c518
James Wigg <92392145+S2JNobody@users.noreply.github.com>
2022-01-21 22:12:02 +0000

    Finished the comments in the sorting algorithm files

--- SelectionSort.java
+++ SelectionSort.java
@@ -12,20 +12,24 @@ public class SelectionSort extends Sorter {
 
     for (int i = 0; i < arr.length - 1; i++) {
       for (int j = i + 1; j < arr.length; j++) {
+
+        // Used for swapping
         int temp = arr[i];
 
         if (order.equals("ascending")) {
+
+          //If this value is smaller than our current smallest value, move it to index j
           this.lastNumSortComparisons++;
           if (arr[i] > arr[j]) {
-            this.lastNumSortComparisons++;
             arr[i] = arr[j];
             arr[j] = temp;
           }
         }
         else {
+
+          //If this value is bigger than our current smallest value, move it to index j
           this.lastNumSortComparisons++;
           if (arr[i] < arr[j]) {
-            this.lastNumSortComparisons++;
             arr[i] = arr[j];
             arr[j] = temp;
           }
