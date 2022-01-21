6a62fea4cf698290136dedcc2083d1e91ef37132
BrAdLy HaRpEr <92391430+TheTamTM@users.noreply.github.com>
2022-01-21 02:44:48 +0000

    updated indentation

--- BubbleSort.java
+++ BubbleSort.java
@@ -13,19 +13,19 @@ public class BubbleSort extends Sorter {
 
         for (int i = 0; i < arr.length - 1; i++) {
           for (int j = 0; j < arr.length - i - 1; j++) {
-			
-      	int temp = arr[j];
-
-				if (order.equals("ascending")) {
-					if (arr[j] > arr[j + 1]) {
-						arr[j] = arr[j + 1];
-						arr[j + 1] = temp;
-					}
+      
+			      int temp = arr[j];
+      
+			    	if (order.equals("ascending")) {
+				    	if (arr[j] > arr[j + 1]) {
+					     	arr[j] = arr[j + 1];
+					    	arr[j + 1] = temp;
+				 	}
 				}
-				else {
-					if (arr[j] < arr[j + 1]) {
-						arr[j] = arr[j + 1];
-						arr[j + 1] = temp;
+			    	else {
+				    	if (arr[j] < arr[j + 1]) {
+					  	  arr[j] = arr[j + 1];
+						    arr[j + 1] = temp;
 					}
 				}
 				
