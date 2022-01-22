23514f4b7987105eecd764f311e303466120454c
David Chu <92391420+ngkhanggg@users.noreply.github.com>
2022-01-22 05:18:21 +0000

    Added a string representation that fits the GUI

--- Sorter.java
+++ Sorter.java
@@ -1,15 +1,20 @@
 public abstract class Sorter {
+    protected long executionTime;
+    protected int comparisons;
 
-    long lastSortSpeed;
-    int lastNumSortComparisons;
+    public abstract String getName();
 
     public abstract int[] sort(int[] unsortedArr, String order);
 
-    public long getLastExecutionTime() {
-      return lastSortSpeed;
+    public long getExecutionTime() {
+        return executionTime;
     }
 
-    public int getLastNumOfComparisons() {
-      return lastNumSortComparisons;
+    public int getComparisons() {
+        return comparisons;
+    }
+
+    public String stringForGUI() {
+        return String.format("<html>%s<br/>- ran in %s milliseconds<br/>- used %s comparisons</html>", getName(), getExecutionTime(), getComparisons());
     }
 }
