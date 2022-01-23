4ea021b7e749a98412d16f4553dc757cf2df744c
Shaydas1 <jameswigg04@gmail.com>
2022-01-23 00:36:51 -0500

    Revert "Merge branch 'Code-Organization' into main"
    This reverts commit 976a2ca78d987c675733cd708a26294565af7dd0, reversing
    changes made to c9cdd5feb8818b62374602e2aeb43c39219be959.

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,40 +1,45 @@
 public class SelectionSort extends Sorter {
-  public String getName() {
-      return "Selection Sort";
-  }
-  
-  public int[] internalSort(int[] unsortedArr, String order) {
-
-    // clone/copy elements from the original array
-    int[] arr = unsortedArr.clone();
-
-    for (int i = 0; i < arr.length - 1; i++) {
-      for (int j = i + 1; j < arr.length; j++) {
-
-        // Used for swapping
-        int temp = arr[i];
-
-        if (order.equals("ascending")) {
-
-          //If this value is smaller than our current smallest value, move it to index j
-          this.comparisons++;
-          if (arr[i] > arr[j]) {
-            arr[i] = arr[j];
-            arr[j] = temp;
-          }
-        }
-        else {
-
-          //If this value is bigger than our current smallest value, move it to index j
-          this.comparisons++;
-          if (arr[i] < arr[j]) {
-            arr[i] = arr[j];
-            arr[j] = temp;
-          }
-        }
-      }
+    public String getName() {
+        return "Selection Sort";
     }
+    
+    public int[] sort(int[] unsortedArr, String order) {
+        // clone the unsorted array
+        int[] arr = unsortedArr.clone();
 
-    return arr;
-  }
+        long start = System.currentTimeMillis(); // start time
+
+        this.comparisons = 0;
+
+        // algorithm
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int temp = arr[i];
+
+                // ascending order
+                if (order.equals("Ascending")) {
+                    if (arr[i] > arr[j]) {
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                    this.comparisons++;
+                }
+
+                // descending order
+                else {
+                    if (arr[i] < arr[j]) {
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                    this.comparisons++;
+                }
+            }
+        }
+
+        long end = System.currentTimeMillis(); // end time
+
+        this.executionTime = end - start; // duration
+
+        return arr;
+    }
 }
